c826a3b0e780ef7d593ec9e3d2adfa32c05c9f19
brentzc <brentzc@gmail.com>
2014-09-30 21:24:02 -0400

    UI skeleton
    This is just after a few hours working on UI stuff. It looks really bad
    but there's obviously a lot of placeholders

---
+++ PlayBoiler/src/cs307/team7/playboiler/FrontPageFragment.java
@@ -0,0 +1,43 @@
+package cs307.team7.playboiler;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FrontPageFragment extends Fragment {
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static FrontPageFragment newInstance(int sectionNumber) {
+        FrontPageFragment fragment = new FrontPageFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public FrontPageFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+    	
+    	
+        View rootView = inflater.inflate(R.layout.front_page, container, false);
+        //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        //textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    	
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        //((MainActivity) activity).onSectionAttached(
+                //getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
---
+++ PlayBoiler/src/cs307/team7/playboiler/SearchFragment.java
@@ -0,0 +1,39 @@
+package cs307.team7.playboiler;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+	public static SearchFragment newInstance(int sectionNumber) {
+        SearchFragment fragment = new SearchFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public SearchFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+    	
+    	
+        View rootView = inflater.inflate(R.layout.search, container, false);
+        //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        //textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    	
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        //((MainActivity) activity).onSectionAttached(
+                //getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
