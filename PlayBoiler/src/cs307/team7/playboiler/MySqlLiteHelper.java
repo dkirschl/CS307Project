bab8be43d9f1cdd21055f695d7d0a07d3dc5c4c7
dkirschl <dkirschl@purdue.edu>
2014-10-15 16:15:35 -0400

    SQL ADDITIONS 2
    Second SQL Additions

--- PlayBoiler/src/cs307/team7/playboiler/MySqlLiteHelper.java
+++ PlayBoiler/src/cs307/team7/playboiler/MySqlLiteHelper.java
@@ -19,17 +19,19 @@ public class MySqlLiteHelper extends SQLiteOpenHelper
 		super(context, DATABASE_NAME, null, DATBASE_VERSION);
 	}
 	
+	@Override
 	public void onCreate(SQLiteDatabase db)
 	{
 		String CREATE_USER_PROFILE_TABLE = "CREATE TABLE user_profile (" + "id INTEGER PRIMARY KEY, " + 
 				"name TEXT, " + "alias TEXT, " + "age INTEGER, " + "gender TEXT, " + "description TEXT, " +
 				"proficiencies TEXT";
-		String CREATE_GAMES_TABLE = "CREATE TABLE past_games (" + "id INTEGER PRIMARY KEY, " + "sport TEXT, " + "location TEXT, " + 
+		String CREATE_GAMES_TABLE = "CREATE TABLE past_games (" + "id INTEGER PRIMARY KEY AUTOINCREMENT, " + "sport TEXT, " + "location TEXT, " + 
 				"date TEXT, " + "time TEXT, " + "title TEXT, " + "creating_user TEXT, " + "attending_ind INTEGER)";
 		db.execSQL(CREATE_GAMES_TABLE);
 		db.execSQL(CREATE_USER_PROFILE_TABLE);
 	}
 	
+	@Override
 	public void onUpgrade(SQLiteDatabase db, int olderVersion, int newVersion)
 	{
 		db.execSQL("DROP TABLE IF EXISTS user_profile");
@@ -53,13 +55,14 @@ public class MySqlLiteHelper extends SQLiteOpenHelper
 	public static final String GAMES_TABLE = "past_games";
 	public static final String GAMES_KEY = "id";
 	public static final String GAMES_SPORT = "sport";
+	public static final String GAMES_LOCATION = "location";
 	public static final String GAMES_DATE = "date";
 	public static final String GAMES_TIME = "time";
 	public static final String GAMES_TITLE = "title";
 	public static final String GAMES_CREATING_USER = "creating_user";
 	public static final String GAMES_ATTENDING_IND = "attending_ind";
 	
-	public static final String[] GAMES_COLUMNS = {GAMES_KEY, GAMES_SPORT, GAMES_DATE, GAMES_TIME,  GAMES_TITLE, 
+	public static final String[] GAMES_COLUMNS = {GAMES_KEY, GAMES_SPORT, GAMES_LOCATION, GAMES_DATE, GAMES_TIME,  GAMES_TITLE, 
 		GAMES_CREATING_USER, GAMES_ATTENDING_IND};
 	
 	public void addEvent(Event event)
@@ -67,8 +70,10 @@ public class MySqlLiteHelper extends SQLiteOpenHelper
 		SQLiteDatabase db = this.getWritableDatabase();
 		ContentValues values = new ContentValues();
 		
+		values.put(GAMES_KEY, event.getKey());
 		values.put(GAMES_SPORT, event.getSport());
 		values.put(GAMES_DATE, event.getDate());
+		values.put(GAMES_LOCATION, event.getLocation());
 		values.put(GAMES_TIME, event.getTime());
 		values.put(GAMES_TITLE, event.getTitle());
 		values.put(GAMES_CREATING_USER, event.getCreating_user());
@@ -124,14 +129,65 @@ public class MySqlLiteHelper extends SQLiteOpenHelper
 		return return_user;
 	}
 	
-	public List<Event> getAllEvents()
+	public List<Event> getPreviousEvents()
 	{
 		List<Event> events = new LinkedList<Event>();
 		
-		String query = "SELECT * FROM " + GAMES_TABLE;
-		SQLiteDatabase db = this.getWritableDatbase();
+		String query = "SELECT * FROM " + GAMES_TABLE + " WHERE " + GAMES_ATTENDING_IND + "=0";
+		SQLiteDatabase db = this.getWritableDatabase();
 		
+		Cursor cursor = db.rawQuery(query, null);
+		Event event = null;
+		
+		if(cursor.moveToFirst())
+		{
+			int x = 0;
+			do
+			{
+				event = new Event();
+				event.setKey(Integer.parseInt(cursor.getString(0)));
+				event.setSport(cursor.getString(1));
+				event.setLocation(cursor.getString(2));
+				event.setDate(cursor.getString(3));
+				event.setTime(cursor.getString(4));
+				event.setTitle(cursor.getString(5));
+				event.setCreating_user(cursor.getString(6));
+				event.setAttending_ind(Integer.parseInt(cursor.getString(7)));
+				events.add(event);
+				x++;
+			}while(x < 5 && cursor.moveToNext());
+		}
 		return events;
 	}
-	
+	public List<Event> getFutureEvents()
+	{
+		List<Event> events = new LinkedList<Event>();
+		
+		String query = "SELECT * FROM " + GAMES_TABLE + " WHERE " + GAMES_ATTENDING_IND + "=1";
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		Cursor cursor = db.rawQuery(query, null);
+		Event event = null;
+		
+		if(cursor.moveToFirst())
+		{
+			int x = 0;
+			do
+			{
+				event = new Event();
+				event.setKey(Integer.parseInt(cursor.getString(0)));
+				event.setSport(cursor.getString(1));
+				event.setLocation(cursor.getString(2));
+				event.setDate(cursor.getString(3));
+				event.setTime(cursor.getString(4));
+				event.setTitle(cursor.getString(5));
+				event.setCreating_user(cursor.getString(6));
+				event.setAttending_ind(Integer.parseInt(cursor.getString(7)));
+				events.add(event);
+				x++;
+			}while(x < 5 && cursor.moveToNext());
+		}
+		return events;
+	}
+
 }
