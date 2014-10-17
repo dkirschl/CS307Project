4bb6e1378597cfcce221981ec9bbd1cb51ac8452
brentzc <brentzc@gmail.com>
2014-10-17 14:46:27 -0400

    Custom Search

--- PlayBoiler/src/cs307/team7/playboiler/SearchFragment.java
+++ PlayBoiler/src/cs307/team7/playboiler/SearchFragment.java
@@ -16,6 +16,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -42,6 +43,8 @@ public class SearchFragment extends Fragment {
     	
         View rootView = inflater.inflate(R.layout.search, container, false);
         final LinearLayout contain = (LinearLayout) rootView.findViewById(R.id.searchContainer);
+        final EditText searchSport = (EditText) rootView.findViewById(R.id.searchSport);
+        final EditText searchLocation = (EditText) rootView.findViewById(R.id.searchLocation);
         final TextView tvDate = (TextView) rootView.findViewById(R.id.searchEventDate);
         final TextView tvTime = (TextView) rootView.findViewById(R.id.searchEventTime);
         titles = new ArrayList<String>();
@@ -94,15 +97,24 @@ final TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDial
 			
 			@Override
 			public void onClick(View v) {
+				String sport = searchSport.getText().toString();
+				String location = searchLocation.getText().toString();
+				String date = tvDate.getText().toString();
+				String time = tvTime.getText().toString();
+				
 				StringBuilder str = new StringBuilder();
 				str.append("/gtev/");
-				addSpaces(str, 30);
+				str.append(sport);
+				addSpaces(str, 30-sport.length());
 				str.append("/");
-				addSpaces(str, 30);
+				str.append(location);
+				addSpaces(str, 30-location.length());
 				str.append("/");
-				addSpaces(str, 10);
+				str.append(date);
+				addSpaces(str, 10-date.length());
 				str.append("/");
-				addSpaces(str, 5);
+				str.append(time);
+				addSpaces(str, 5-time.length());
 				str.append("/");
 				addSpaces(str, 1);
 				str.append("/");
@@ -118,7 +130,6 @@ final TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDial
 					e1.printStackTrace();
 				}
 				Log.d("Search Event", result);
-				//DenverAlgorithm(result);
 				String[][] s = split(result);
 				for (int i = 0; i < s.length; i++) {
 					for (int j = 0; j < s[i].length; j++) {
@@ -136,18 +147,7 @@ final TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDial
 				}
 				
 				
-				/*
-				int j = 20;
-		        for (int i= 0; i < j; i++) {
-		        	View ll = inflater.inflate(R.layout.event_view, container, false);
-		        	TextView tv = (TextView) ll.findViewById(R.id.searchEventTitle);
-		        	TextView tv2 = (TextView) ll.findViewById(R.id.searchEventDate);
-		        	tv.setText("Title : " + i);
-		        	tv2.setText("Date : " + i);
-		        	contain.addView(ll);
-		        	
-		        }
-		        */
+				
 			}
 		});
         
