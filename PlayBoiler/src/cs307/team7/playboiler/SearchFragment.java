e2338942624447ea42673b165df29b4fefed3ab6
brentzc <brentzc@gmail.com>
2014-10-16 22:51:16 -0400

    Added search fields

--- PlayBoiler/src/cs307/team7/playboiler/SearchFragment.java
+++ PlayBoiler/src/cs307/team7/playboiler/SearchFragment.java
@@ -1,7 +1,9 @@
 package cs307.team7.playboiler;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.app.TimePickerDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +12,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class SearchFragment extends Fragment {
 	public static SearchFragment newInstance(int sectionNumber) {
@@ -20,6 +24,9 @@ public class SearchFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+	
+	Button dateSelect, timeSelect;
+	int m,h;
 
     public SearchFragment() {
     }
@@ -31,6 +38,51 @@ public class SearchFragment extends Fragment {
     	
         View rootView = inflater.inflate(R.layout.search, container, false);
         final LinearLayout contain = (LinearLayout) rootView.findViewById(R.id.searchContainer);
+        final TextView tvDate = (TextView) rootView.findViewById(R.id.searchEventDate);
+        final TextView tvTime = (TextView) rootView.findViewById(R.id.searchEventTime);
+        
+        dateSelect = (Button) rootView.findViewById(R.id.searchSelectDate);
+    	dateSelect.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				final Dialog d = new Dialog(v.getContext());
+				d.setContentView(R.layout.pick_date);
+				d.setTitle("Pick a Date");
+				
+				final DatePicker dp = (DatePicker) d.findViewById(R.id.datePicker);
+				Button done = (Button) d.findViewById(R.id.dateDone);
+				done.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v1) {
+						StringBuilder date = new StringBuilder().append((dp.getMonth() + 1)).append("-").append(dp.getDayOfMonth()).append("-").append(dp.getYear());
+						tvDate.setText(date);
+						d.cancel();
+					}
+				});
+				d.show();
+			}
+    	});
+    	
+final TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
+			
+			@Override
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				h = hourOfDay;
+				m = minute;
+				tvTime.setText(new StringBuilder().append(timeFix(h)).append(":").append(timeFix(m)));
+			}
+		};
+    	timeSelect = (Button) rootView.findViewById(R.id.searchSelectTime);
+    	timeSelect.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TimePickerDialog tpd = new TimePickerDialog(v.getContext(), timePickerListener, h, m, false);
+				tpd.show();
+			}
+		});
         Button search = (Button) rootView.findViewById(R.id.searchButton);
         search.setOnClickListener(new OnClickListener() {
 			
@@ -96,5 +148,13 @@ public class SearchFragment extends Fragment {
 		}
     	return sb;
     	
+    }
+    
+    private static String timeFix(int c) {
+    	if (c >= 10) {
+    		return String.valueOf(c);
+    	} else {
+    		return "0" + String.valueOf(c);
+    	}
     }
 }
